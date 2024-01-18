61be493c03176c9cdb78c672a7700d91f59ed7b1
mmm1245 <m.morhac123@gmail.com>
2024-01-18 19:11:31 +0100

    source file parsing

--- bbscript/src/lib.rs
+++ bbscript/src/lib.rs
@@ -3,79 +3,12 @@ mod ast;
 mod eval;
 mod variant;
 
+use crate::eval::Function;
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub syntax);
 
-#[cfg(test)]
-mod tests {
-    use crate::ast;
-    use crate::ast::{Expression, Statement, StatementBlock};
-    use crate::eval::{ExecutionEnvironment, Function, ScriptError};
-    use crate::variant::Variant;
-    use dyn_eq::DynEq;
-    use immutable_string::ImmutableString;
-    use std::any::{Any, TypeId};
-    use std::cell::RefCell;
-
-    #[test]
-    fn test() {
-        let mut environment = ExecutionEnvironment::new();
-        environment.register_global_function("do_something", |params| {
-            Ok((Variant::Primitive(Box::<ImmutableString>::new("aaa".into()))))
-        });
-        environment.register_global_function("print", |params| {
-            let output = params
-                .into_iter()
-                .map(|param| match param {
-                    Variant::Primitive(string) => {
-                        let string = string.as_ref();
-                        if string.type_id() == TypeId::of::<ImmutableString>() {
-                            Ok(string
-                                .as_any()
-                                .downcast_ref::<ImmutableString>()
-                                .unwrap()
-                                .to_string())
-                        } else {
-                            Err(())
-                        }
-                    }
-                    _ => Err(()),
-                })
-                .collect::<Result<String, ()>>();
-            match output {
-                Ok(str) => {
-                    println!("{str}");
-                    Ok(Variant::Null)
-                }
-                Err(_) => Err(ScriptError::RuntimeError("cannot print non string".into())),
-            }
-        });
-        Function {
-            name: "aaa".into(),
-            body: StatementBlock {
-                statements: vec![Statement::Eval {
-                    expression: Expression::Call {
-                        expression: Box::new(Expression::ScopedVariable {
-                            name: "print".into(),
-                        }),
-                        parameters: vec![Expression::Call {
-                            expression: Box::new(Expression::ScopedVariable {
-                                name: "do_something".into(),
-                            }),
-                            parameters: vec![],
-                        }],
-                    },
-                }],
-            },
-            parameter_names: vec![],
-        }
-        .run(Variant::Null, vec![], &environment);
-
-        println!(
-            "{:?}",
-            crate::syntax::StatementParser::new()
-                .parse("ahoj = 6.0-'ahoj';")
-                .unwrap()
-        );
-    }
+pub fn parse_source_file(file: &str) -> Result<Vec<Function>, String> {
+    crate::syntax::SourceFileParser::new()
+        .parse(file)
+        .map_err(|error| error.to_string())
 }
